d9216970ece53d725dfbfe219576424a58621eea
Brimix <brian.morris.e97@gmail.com>
2020-11-29 02:34:35 -0300

    Renamed: score -> result

--- src/main/java/BrimixGaming/BraBreBrimix/Model/Score.java
+++ src/main/java/BrimixGaming/BraBreBrimix/Model/Score.java
@@ -12,7 +12,7 @@ public class Score {
     @GenericGenerator(name = "native", strategy = "native")
     private long id;
     private Date ended;
-    private char score;
+    private char result;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name="PlayerID")
@@ -25,9 +25,9 @@ public class Score {
     public Score() {
         this.ended = new Date();
     }
-    public Score(char score, Player player, Game game) {
+    public Score(char result, Player player, Game game) {
         this();
-        this.score = score;
+        this.result = result;
         player.addScore(this);
         game.addScore(this);
     }
@@ -35,8 +35,8 @@ public class Score {
     public long getId() { return id; }
     public Date getEnded() { return ended; }
     public void setEnded(Date ended) { this.ended = ended; }
-    public char getScore() { return score; }
-    public void setScore(char score) { this.score = score; }
+    public char getResult() { return result; }
+    public void setResult(char result) { this.result = result; }
     public Player getPlayer() { return player; }
     public void setPlayer(Player player) { this.player = player; }
     public Game getGame() { return game; }

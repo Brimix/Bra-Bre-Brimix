cf041bd985415c69aaad3fb495cd473d12896318
Brimix <brian.morris.e97@gmail.com>
2020-12-06 22:30:40 -0300

    Updated findByUsername method

--- src/main/java/BrimixGaming/BraBreBrimix/Repository/PlayerRepository.java
+++ src/main/java/BrimixGaming/BraBreBrimix/Repository/PlayerRepository.java
@@ -2,8 +2,13 @@ package BrimixGaming.BraBreBrimix.Repository;
 
 import BrimixGaming.BraBreBrimix.Model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+import java.util.Optional;
 
 @RepositoryRestResource
 public interface PlayerRepository extends JpaRepository<Player, Long> {
+    Optional<Player> findByUsername(@Param("username") String username);
 }

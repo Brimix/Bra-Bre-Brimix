c85b141b90be451e62911c8b8d6cbaa50c679063
Brimix <brian.morris.e97@gmail.com>
2020-12-06 22:30:06 -0300

    Updated permissions

--- src/main/java/BrimixGaming/BraBreBrimix/BraBreBrimixApplication.java
+++ src/main/java/BrimixGaming/BraBreBrimix/BraBreBrimixApplication.java
@@ -8,11 +8,30 @@ import BrimixGaming.BraBreBrimix.Repository.GamePlayerRepository;
 import BrimixGaming.BraBreBrimix.Repository.GameRepository;
 import BrimixGaming.BraBreBrimix.Repository.PlayerRepository;
 import BrimixGaming.BraBreBrimix.Repository.ScoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.WebAttributes;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+//import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter
 
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.Arrays;
 import java.util.List;
 
@@ -32,9 +51,9 @@ public class BraBreBrimixApplication {
 			ScoreRepository s_rep
 	){
 		return (args) -> {
-			 Player P1 = new Player("April");
-			 Player P2 = new Player("Brian");
-			 Player P3 = new Player("Carol");
+			 Player P1 = new Player("April", passwordEncoder().encode("april"));
+			 Player P2 = new Player("Brian", passwordEncoder().encode("brian"));
+			 Player P3 = new Player("Carol", passwordEncoder().encode("carol"));
 			 Game G1 = new Game();
 			 Game G2 = new Game();
 			 Game G3 = new Game();
@@ -59,5 +78,71 @@ public class BraBreBrimixApplication {
 					 		.filter(s -> s != null)
 					 		.collect(toList()));
 		};
+	}
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+}
+
+@Configuration
+class WebSecurityConfiguration extends GlobalAuthenticationConfigurerAdapter {
+	@Autowired
+	PlayerRepository playerRepository;
+
+	@Override
+	public void init(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(inputName-> {
+			Player player = playerRepository.findByUsername(inputName).orElse(null);
+			if (player != null) {
+				System.out.println("Player found!\n");
+				return new User(player.getUsername(), player.getPassword(),
+						AuthorityUtils.createAuthorityList("USER"));
+			} else {
+				System.out.println("Player not found!\n");
+				throw new UsernameNotFoundException("Unknown user: " + inputName);
+			}
+		});
+	}
+}
+
+@EnableWebSecurity
+@Configuration
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/web/login.html").permitAll()
+			.antMatchers("/web/js/login.js").permitAll()
+			.antMatchers("/web/js/jquery.js").permitAll()
+			.antMatchers("/web/**").hasAuthority("USER")
+			.antMatchers("/api/**").permitAll()
+			.antMatchers("/**").permitAll();
+		http.formLogin()
+			.usernameParameter("name")
+			.passwordParameter("pwd")
+			.loginPage("/api/login");
+		http.logout().logoutUrl("/api/logout");
+
+		// turn off checking for CSRF tokens
+		http.csrf().disable();
+
+		// if user is not authenticated, just send an authentication failure response
+		http.exceptionHandling().authenticationEntryPoint((req, res, exc) -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED));
+
+		// if login is successful, just clear the flags asking for authentication
+		http.formLogin().successHandler((req, res, auth) -> clearAuthenticationAttributes(req));
+
+		// if login fails, just send an authentication failure response
+		http.formLogin().failureHandler((req, res, exc) -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED));
+
+		// if logout is successful, just send a success response
+		http.logout().logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler());
+	}
+	private void clearAuthenticationAttributes(HttpServletRequest request) {
+		HttpSession session = request.getSession(false);
+		if (session != null) {
+			session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+		}
 	}
 }
